1990f34d89c3f016cf6315ecba0a5c9479cfa2ac
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-15 14:11:24 -0600

    Notes and exercises from 4-15 morning

--- 4-15 #2.py
+++ 4-15 #2.py
@@ -19,3 +19,25 @@ class Employee(object):
 
 
 mw = Employee("Morgan Williams", 24, "SD", "a shitload")
+
+
+# or using def __str__
+
+
+class Job(object):
+    def __init__(self, position, salary):
+        self.position = position
+        self.salary = salary
+
+class Employee(object):
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.job = Job(position, salary)
+
+    def __str__(self):
+        return "My name is {}, I am {} years old and I am a {} and I make {}.".format(self.name, self.age, self.job.position, self.job.salary)
+
+
+mw = Employee("Morgan Williams", 24, "SD", "a shitload")
+print mw
