0edc270a3d0bec588a311e031e77b6aab57c2294
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-29 10:19:52 -0600

    Updated with Make Bricks and Make Chocolate exercises

--- CodingBat practice.py
+++ CodingBat practice.py
@@ -312,4 +312,66 @@
 
 
 
+# Make bricks practice problem
+# Go through my fail cases and have true at the end.
+# - if the total number of small bricks i had + big bricks
+# - if the left over of big bricks
+
+# def make_bricks(small, big, goal):
+#     if (small + (big * 5)) < goal:
+#         return False
+#     elif goal % 5 > small:
+#         return False
+#     else:
+#         return True
+#
+#
+# print make_bricks(4, 1, 9)
+# print make_bricks(4, 1, 10)
+# print make_bricks(4, 1, 7)
+
+
+
+
+
+
+# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each).
+# Return the number of small bars to use, assuming we always use big bars before small bars.
+# Return -1 if it can't be done.
+#
+# make_chocolate(4, 1, 9)  4
+# make_chocolate(4, 1, 10)  -1
+# make_chocolate(4, 1, 7)  2
+# make_chocolate(6, 1, 11) 6
+
+
+
+def make_chocolate(small, big, goal):
+    if (small + (big * 5)) < goal:
+        return -1
+    elif (goal % 5) + small >= goal:
+        return (small) - ((goal)-goal%5)
+    elif (goal % 5) + small == goal:
+        return small
+    elif goal % 5 < small:
+        return goal % 5
+    elif goal % 5 > small:
+        return goal - (big*(goal%5))
+
+
+
+    # elif goal % 5 > small:
+    #     return -1
+
+print make_chocolate(4, 1, 9)
+print make_chocolate(4, 1, 10)
+print make_chocolate(4, 1, 7)
+print make_chocolate(6, 1, 11)
+
+
+
+
+
+
+
 
