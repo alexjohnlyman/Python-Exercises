75018b55d87756822c2e0f876c0397d4e21a31f1
Alex Lyman <alexjohnlyman@gmail.com>
2015-05-05 10:16:34 -0600

    Added end of string problem

--- CodingBat practice.py
+++ CodingBat practice.py
@@ -416,6 +416,28 @@
 
 
 
+# Given two strings, return True if either of the strings appears at the very end of the other string,
+# ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+# Note: s.lower() returns the lowercase version of a string.
+#
+# end_other('Hiabc', 'abc')  True
+# end_other('AbC', 'HiaBc')  True
+# end_other('abc', 'abXabc')  True
 
 
+#I need to check to see if a is in b, or if b is in a.
 
+# def end_other(a, b):
+#     a = a.lower()
+#     b = b.lower()
+#
+#     if b in a[-len(b):]:
+#         return True
+#     if a in b[-len(a):]:
+#         return True
+#     else:
+#         return False
+#
+# print end_other('Hiabc', 'abc')
+# print end_other('AbC', 'HiaBc')
+# print end_other('abc', 'abXabc')
